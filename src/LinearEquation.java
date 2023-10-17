9c1cb2065bd4dd1603f4f94144505af51d717025
BT_2N2_23 <sample@bths.edu>
2023-10-17 14:03:12 -0400

    fix bugs later

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,2 +1,67 @@
-package PACKAGE_NAME;public class LinearEquation {
+public class LinearEquation {
+
+    // Instance Variables
+    private int x1;
+    private int y1;
+    private int x2;
+    private int y2;
+
+    // Constructor, initializing variables
+    public LinearEquation(int x1, int y1, int x2, int y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+
+    // Calculates the distance between the two points
+    public double distance() {
+        double x = Math.pow((x2 - x1), 2);
+        double y = Math.pow((y2 - y1), 2);
+        double d = Math.sqrt(x + y);
+        return roundedToHundredth(d);
+    }
+
+    // Calculates and returns the slope of the line
+    public double slope() {
+        double m = (y2 - y1) / (x2 - x1);
+        return roundedToHundredth(m);
+    }
+
+    // Calculates and returns the y-intercept of the line
+    public double yIntercept() {
+        double b = y1 - (slope() * x1);
+        return roundedToHundredth(b);
+    }
+
+    // Calculates and returns the linear equation of the line that goes through given points
+    public String equation() {
+        if (y1 == y2) {
+            return "y = " + y1;
+        }
+        double m = slope();  // 5/4 not 1.25, fix this later
+        double b = yIntercept();
+        return "y = " + m + "x + " + b;
+    }
+
+    // Calculates and returns the y-coordinate on the line when given an x-coordinate
+    public String coordinateForX(double x) {
+        double y = (slope() * x) + yIntercept();
+        return "(" + x + ", " + y + ")";
+    }
+
+    // Returns a string composed of all the information about the lines and points
+    public String lineInfo() {
+        String str = "The two points are: (" + x1 + ", " + y1 + ") and (" + x2 + ", " + y2 + ")";
+        str += "\nThe equation of the line between these points is: " + equation();
+        str += "\nThe slope of this line is: " + slope();
+        str += "\nThe y-intercept of this line is: " + yIntercept();
+        str += "\nThe distance between these points is: " + distance();
+        return str;
+    }
+
+    private double roundedToHundredth(double toRound) {
+        double rounded = Math.round(toRound * 100) / 100.0;
+        return rounded;
+    }
 }
--- src/LinearEquationLogic.java
+++ src/LinearEquationLogic.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class LinearEquationLogic {
+import java.util.Scanner;
+public class LinearEquationLogic {
+    public void start() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Welcome to the linear equation calculator!");
+        System.out.print("Enter coordinate 1: ");
+        String firstCoord = scan.nextLine();
+        System.out.print("Enter coordinate 2: ");
+        String secondCoord = scan.nextLine();
+        System.out.println();
+        String answer = "y";
+        while (!answer.equals("n")) {
+            int x1 = Integer.parseInt(firstCoord.substring(1, firstCoord.indexOf(",")));
+            int y1 = Integer.parseInt(firstCoord.substring(firstCoord.indexOf(",") + 2, firstCoord.indexOf(")")));
+            int x2 = Integer.parseInt(secondCoord.substring(1, secondCoord.indexOf(",")));
+            int y2 = Integer.parseInt(secondCoord.substring(secondCoord.indexOf(",") + 2, secondCoord.indexOf(")")));
+            LinearEquation equation = new LinearEquation(x1, y1, x2, y2);
+            System.out.println(equation.lineInfo());
+            System.out.println();
+            System.out.print("Enter a value for x: ");
+            double testX = scan.nextDouble();
+            System.out.println("The point on the line is : " + equation.coordinateForX(testX));
+            System.out.println();
+            System.out.print("Would you like to enter another pair of coordinates? y/n: ");
+            answer = scan.nextLine();
+        }
+        System.out.println("Thank you for using the slope calculator, goodbye!");
+    }
 }
